8750044f3989d6210b94c7c1c69f91517b74df44
Misra <subhajitmisra20@gmail.com>
2024-04-07 21:55:00 +0530

    Commit 3

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @Configuration
+@ControllerAdvice
 public class Test {
 }
